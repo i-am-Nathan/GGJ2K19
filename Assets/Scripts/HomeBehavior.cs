5455b91b2f78ca1aafb84b6f7ce54916317b5777
Nathan SItu <nathansitu@hotmail.com>
2019-01-26 13:15:26 +1300

    Added Gamemanager and scenes

---
+++ Assets/Scripts/HomeBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeBehavior : MonoBehaviour {
+
+    //delegates
+    public delegate void OnGoalMet();
+
+    //events
+    public event OnGoalMet VictoryTriggered;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player") {
+            VictoryTriggered();
+        }
+    }
+}
