fdc66382d74baf5b54d60e96108d3ffbb1ff7284
JunoJin <jjin525@aucklanduni.ac.nz>
2019-01-26 16:02:36 +1300

    I added a playerHealth script and UI element to match
    just a basic slider; being in bat form decrements health slowly

---
+++ Assets/CharacterHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterHealth : MonoBehaviour {
+	public float startingHealth = 100f;
+	public float currentHealth;
+	public Slider healthSlider;
+
+	CharacterMovement characterMovement;
+	CharacterMorph characterMorph;
+	bool isDead;
+
+	// Use this for initialization
+	void Start () {
+		characterMovement = GetComponent <CharacterMovement> ();
+		characterMorph = GetComponent <CharacterMorph> ();
+		currentHealth = startingHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void TakeDamage (float amount) {
+		currentHealth -= amount;
+		healthSlider.value = currentHealth;
+	}	
+}
